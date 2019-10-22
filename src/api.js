d6bb09886884acef3055b38f3a77fc6c317ea84c
Yurii Holiuk <bionicuss@gmail.com>
2019-10-22 17:44:25 +0300

    upgrade request

--- src/api.js
+++ src/api.js
@@ -1,11 +1,30 @@
 import { FetchError } from './FetchError.js';
 
 export function createAPI(baseUrl) {
-  const makeRequest = (url, method = 'GET', data) => {
-    return fetch(`${baseUrl}/${url}`, {
-      method,
-      ...(data ? { body: JSON.stringify(data) } : {})
-    })
+  const makeRequest = (method, url, data, options = {}) => {
+    const init = { method };
+
+    if (data) {
+      init.body = JSON.stringify(data);
+    }
+
+    if (options.headers) {
+      init.headers = options.headers;
+    }
+
+    let queryStr = '';
+
+    if (options.query) {
+      queryStr = Object.entries(options.query)
+        .map(([key, value]) => `${key}=${value}`)
+        .join('&');
+
+      if (queryStr) {
+        queryStr = `?${queryStr}`;
+      }
+    }
+
+    return fetch(`${baseUrl}/${url}${queryStr}`, init)
       .then(res => {
         if (!res.ok) {
           return res.json()
@@ -22,20 +41,20 @@ export function createAPI(baseUrl) {
   };
 
   return {
-    get(url) {
-      return makeRequest(url);
+    get(url, options) {
+      return makeRequest('GET', url, null, options);
     },
-    post(url, data) {
-      return makeRequest(url, 'POST', data);
+    post(url, data, options) {
+      return makeRequest('POST', url, data, options);
     },
-    patch(url, data) {
-      return makeRequest(url, 'PATCH', data);
+    patch(url, data, options) {
+      return makeRequest('PATCH', url, data, options);
     },
-    put(url, data) {
-      return makeRequest(url, 'PUT', data);
+    put(url, data, options) {
+      return makeRequest('PUT', url, data, options);
     },
-    delete(url, data) {
-      return makeRequest(url, 'DELETE', data);
+    delete(url, data, options) {
+      return makeRequest('DELETE', url, data, options);
     }
   };
 }
